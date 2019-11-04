d120fc9ee9babce21c3b97329331ed02465fadd8
suhailiib <40245947+suhailiib@users.noreply.github.com>
2019-11-04 16:11:25 -0600

    MapFragment added

--- app/src/main/java/edu/uark/csce/minimap/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uark/csce/minimap/ui/home/HomeFragment.java
@@ -1,10 +1,16 @@
 package edu.uark.csce.minimap.ui.home;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,22 +19,62 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import edu.uark.csce.minimap.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener  {
 
     private HomeViewModel homeViewModel;
+    private GoogleMap map;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+//        homeViewModel =
+//                ViewModelProviders.of(this).get(HomeViewModel.class);
+//        View root = inflater.inflate(R.layout.fragment_home, container, false);
+////        final TextView textView = root.findViewById(R.id.text_home);
+////        homeViewModel.getText().observe(this, new Observer<String>() {
+////            @Override
+////            public void onChanged(@Nullable String s) {
+////                textView.setText(s);
+////            }
+////        });
+//        return root;
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //get the fragment xml resources
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //assign the local map varaible a googleMap variable
+        map = googleMap;
+        //set up an onMarkClick listener
+        map.setOnMarkerClickListener(this);
+        //add the marker and setup the camera
+//        map.addMarker(option);
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,  50));
+        //set up current location button and marker
+//        map.setMyLocationEnabled(true);
+//        map.setOnMyLocationButtonClickListener(this);
+        //update the map with current photos and locations
+//        updateMap(this.latLngStringList);
+    }
+
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
     }
 }
