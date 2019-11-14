52317ca9ba425d6aa87395c136e5b5bc0d07939f
rtsorge <RTSorge@email.uark.edu>
2019-11-13 22:04:24 -0600

    Implemented polygons on the map and color shading for the heat map of compatible buildings

--- app/src/main/java/edu/uark/csce/minimap/Building.java
+++ app/src/main/java/edu/uark/csce/minimap/Building.java
@@ -1,17 +1,30 @@
 package edu.uark.csce.minimap;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class Building
 {
     String buildingName;
     double latitude;
     double longitude;
+    LatLng buildingLocation;
     boolean heatmapAvailable;
+    LatLng[] polygon;
 
     public Building(String buildingName, double latitude, double longitude, boolean heatmapAvailable) {
         this.buildingName = buildingName;
         this.latitude = latitude;
         this.longitude = longitude;
+        buildingLocation = new LatLng(latitude, longitude);
         this.heatmapAvailable = heatmapAvailable;
+    }
+
+    public Building(String buildingName, double latitude, double longitude, boolean heatmapAvailable, double lat1, double lon1, double lat2, double lon2, double lat3, double lon3, double lat4, double lon4) {
+        this.buildingName = buildingName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.heatmapAvailable = heatmapAvailable;
+        polygon = new LatLng[]{new LatLng(lat1, lon1), new LatLng(lat2, lon2), new LatLng(lat3, lon3), new LatLng(lat4, lon4)};
     }
 
     public String getBuildingName() {
@@ -46,5 +59,19 @@ public class Building
         this.heatmapAvailable = heatmapAvailable;
     }
 
+    public LatLng getBuildingLocation() {
+        return buildingLocation;
+    }
 
+    public void setBuildingLocation(LatLng buildingLocation) {
+        this.buildingLocation = buildingLocation;
+    }
+
+    public LatLng[] getPolygon() {
+        return polygon;
+    }
+
+    public void setPolygon(LatLng[] polygon) {
+        this.polygon = polygon;
+    }
 }
--- app/src/main/java/edu/uark/csce/minimap/MapsActivity.java
+++ app/src/main/java/edu/uark/csce/minimap/MapsActivity.java
@@ -2,27 +2,40 @@ package edu.uark.csce.minimap;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     Building[] buildings = {
-            new Building("Mullins Library", 36.0686, -94.1736, false),
+            new Building("Mullins Library",
+                    36.0686,
+                    -94.1736,
+                    true,
+                    36.069150,
+                    -94.174346,
+                    36.069178,
+                    -94.173322,
+                    36.068206,
+                    -94.173303,
+                    36.068206,
+                    -94.174300),
             new Building("Brough Dining Hall", 36.0662, -94.1752, false),
             new Building("JB-Hunt", 36.066052, -94.173755,false),
             new Building("The Union", 36.082157, -94.171852, false),
             new Building("Pat Walker: Health Center", 36.070790, -94.176020, false),
             new Building("Campus Bookstore on Dickson", 36.066760, -94.167390, false)
-
     };
 
     int position;
@@ -54,6 +67,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker to the selected building location and move the camera
         LatLng buildingLocation = new LatLng(buildings[position].getLatitude(), buildings[position].getLongitude());
         mMap.addMarker(new MarkerOptions().position(buildingLocation).title(buildings[position].getBuildingName()));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(buildingLocation));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(buildingLocation, 13));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(buildingLocation)      // Sets the center of the map to location user
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        if(buildings[position].isHeatmapAvailable())
+        {
+            //Not finished here, need to get the color from the database.
+            int heatShade = Color.argb(150,150, 150, 0);
+            PolygonOptions rectOptions = new PolygonOptions()
+                    .add(
+                            buildings[position].polygon[0],
+                            buildings[position].polygon[1],
+                            buildings[position].polygon[2],
+                            buildings[position].polygon[3]
+                    )
+                    .fillColor(heatShade);
+            Polygon polygon = mMap.addPolygon(rectOptions);
+        }
     }
 }
