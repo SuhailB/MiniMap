1adf9913170e078ec4cfe2eedb571394d4c9dbdd
Kwangkyu Choi <kwangkyuchoi@kwangkyus-mbp.ddns.uark.edu>
2019-11-19 16:43:40 -0600

    comment out twon lines that causes crash

--- app/src/main/java/edu/uark/csce/minimap/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/uark/csce/minimap/ui/notifications/NotificationsFragment.java
@@ -24,8 +24,8 @@ public class NotificationsFragment extends Fragment {
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        int num = getArguments().getInt("POS");
-        Log.e("POS", String.valueOf(num));
+//        int num = getArguments().getInt("POS");
+//        Log.e("POS", String.valueOf(num));
         notificationsViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
