bc457383e17c8b93d59de4520473782e34c01533
rtsorge <rtsorge@email.uark.edu>
2019-12-06 16:02:41 -0600

    Reset branch

--- app/src/main/java/edu/uark/csce/minimap/ui/dashboard/BuildingDetailsFragment.java
+++ app/src/main/java/edu/uark/csce/minimap/ui/dashboard/BuildingDetailsFragment.java
@@ -1,42 +1,21 @@
 package edu.uark.csce.minimap.ui.dashboard;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import edu.uark.csce.minimap.ColorService;
 import edu.uark.csce.minimap.R;
 
 public class BuildingDetailsFragment extends DialogFragment {
 
     private Button bviewMap;
-    private int count;
-    private TextView textView;
-    public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // Get extra data included in the Intent
-            count = intent.getIntExtra("device_count", 0);
-
-            textView.setText(count+" People");
-//            updateBuildingColor(map);
-            Log.e("receiver", "Got message: " + count);
-        }
-    };
 
     public BuildingDetailsFragment() {
         // Empty constructor is required for DialogFragment
@@ -56,7 +35,6 @@ public class BuildingDetailsFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mMessageReceiver, new IntentFilter(ColorService.ACTION));
 //        return inflater.inflate(R.layout.fragment_home, container, false);
         return inflater.inflate(R.layout.building_details_fragment, container, false);
     }
@@ -77,8 +55,6 @@ public class BuildingDetailsFragment extends DialogFragment {
 
             }
         });
-        textView = (TextView) view.findViewById(R.id.people_count);
-        textView.setText(count+" People");
 
 
     }
